f5470005624c7b005bbc7634a3bc26a5ed111c8f
Iaroslav <iaroslav.nevalenov-udilov@iesruizgijon.com>
2026-02-22 20:29:15 +0100

    Cambios de Spring

--- Spring/proyectoFinal/demo/src/main/java/com/example/demo/entity/User.java
+++ Spring/proyectoFinal/demo/src/main/java/com/example/demo/entity/User.java
@@ -6,6 +6,7 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -29,7 +30,7 @@ public class User {
      * Identificador único del usuario (autogenerado).
      */
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
