1409c977f96233633e5208d1083cbb01e03aa030
Iaroslav <iaroslav.nevalenov-udilov@iesruizgijon.com>
2026-02-23 22:48:34 +0100

    Cambios de Spring

--- Spring/proyectoFinal/demo/src/main/java/com/example/demo/controller/MenuController.java
+++ Spring/proyectoFinal/demo/src/main/java/com/example/demo/controller/MenuController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.entity.Categoria;
 import com.example.demo.repository.CategoriaRepository;
 
 /**
